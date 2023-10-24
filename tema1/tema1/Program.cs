a250ac5dd429d989d04bf81576390941eb3bde84
berindeanudavid <david.berindeanu@emanuel.ro>
2023-10-24 19:09:07 +0300

    user-password

---
+++ tema1/tema1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace tema1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string username = "dutu";
+            const string password = "parola";
+            bool ok = false;
+            while (ok == false)
+            {
+                string u, p;
+                Console.Write("username: ");
+                u = Console.ReadLine();
+                Console.Write("password: ");
+                p = Console.ReadLine();
+                if ((string.Compare(u, username) == 0) && (string.Compare(p, password) == 0))
+                {
+                    ok = true;
+                    Console.WriteLine("Bine ai venit, " + username);
+                }
+                else
+                {
+                    Console.WriteLine("wrong username or password");
+                }
+            }
+        }
+    }
+}
